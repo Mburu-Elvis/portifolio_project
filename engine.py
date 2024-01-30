7067a658ad60287ee49299e9e2d5282b0e752f69
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2024-01-30 23:01:13 +0300

    updates on dashboard page

--- engine.py
+++ engine.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Enum, ARRAY
 from uuid import uuid4
@@ -26,6 +26,32 @@ class Customer(Base, BaseModel):
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(100), nullable=False)
 
+class Product(Base, BaseModel):
+    __tablename__ = 'products'
+    product_id = Column(Integer, nullable=False, primary_key=True)
+    product_name = Column(String(50), nullable=False, unique=True)
+    category_id = Column(Integer, ForeignKey('products_category', back_populates='ProductCategory'), nullable=False)
+    price = Column(Integer, nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+class ProductCategory(Base, BaseModel):
+    __tablename__ = 'products_category'
+    category_id = Column(Integer, nullable=False, primary_key=True)
+    category_name =  Column(String(50), nullable=False)
+
+
+class Order(Base, BaseModel):
+    __tablename__ = 'orders'
+    order_id = Column(String(36), nullable=False, primary_key=True)
+    order_items = Column(String(50), nullable=False)
+    customer_id = Column(String(40), ForeignKey('customers.customer_id'),nullable=False)
+    product_id = Column(Integer, ForeignKey('products.product_id'))
+    location = Column(String(50), nullable=False)
+    number_of_items = Column(Integer, nullable=False)
+    date = Column(DateTime, nullable=False)
+    total_amount = Column(Integer, nullable=False)
+    # customer = relationship("User", back_populates=)
+
 
 class Rider(Base, BaseModel):
     __tablename__ = 'riders'
@@ -37,37 +63,21 @@ class Rider(Base, BaseModel):
     available = Column(Enum('available', 'on-delivery', 'unavailable'), default='available')
 
 
-class Product(Base, BaseModel):
-    __tablename__ = 'products'
-    product_id = Column(Integer, nullable=False, primary_key=True)
-    product_name = Column(String(50), nullable=False, unique=True)
-    category_id = Column(Integer, nullable=False)
-    price = Column(Integer, nullable=False)
-    quantity = Column(Integer, nullable=False)
+class Delivery(Base, BaseModel):
+    __tablename__ = 'delivery'
+    delivery_id = Column(String(40), primary_key=True)
+    date_time = Column(DateTime, nullable=False)
+    customer_id = Column(String(40), ForeignKey('customers.customer_id'))
+    order_id = Column(String(40), ForeignKey('orders.order_id'))
+    payment_message = Column(String(40), nullable=False, unique=True)
 
-
-class Order(Base, BaseModel):
-    __tablename__ = 'orders'
-    order_id = Column(String(36), nullable=False, primary_key=True)
-    order_items = Column(String(50), nullable=False)
-    customer_id = Column(String(40), nullable=False)
-    location = Column(String(50), nullable=False)
-    number_of_items = Column(Integer, nullable=False)
-    date = Column(DateTime, nullable=False)
-    total_amount = Column(Integer, nullable=False)
-
-
-class ProductCategory(Base, BaseModel):
-    __tablename__ = 'products_category'
-    category_id = Column(Integer, nullable=False, primary_key=True)
-    category_name =  Column(String(50), nullable=False)
 
 class Suppliers(Base, BaseModel):
     __tablename__ = 'suppliers'
     supplier_id = Column(Integer, primary_key=True)
     supplier_name = Column(String(50), nullable=False, unique=True)
     location = Column(String(50), nullable=False)
-    products = Column(String(50), nullable=False)
+    #products = Column(String())
 
 
 pwd = '!@mElv!s@19'
